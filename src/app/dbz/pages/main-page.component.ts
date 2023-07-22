c90186a4bae3781ac797be4687fda356bac4963c
CrixL94 <lopezvasquezcristhian@gmail.com>
2023-07-22 16:47:34 -0600

    + feat
    se agregaron varios funcionamientos que como ser la comunicaion entre componenetes

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+  public characters: Character[] = [
+    {
+      name: 'Krilin',
+      power:1000
+    },
+    {
+      name: 'Goku',
+      power:9500
+    },
+    {
+      name: 'Vegeta',
+      power:7500
+    }
+  ];
+
+  onNewCharacterAdd(character: Character): void {
+    // console.log("Main Page")
+    // console.log(character)
+
+    this.characters.push(character);
+  }
+
+  onDelete(index:number){
+    this.characters.splice(index, 1);
+  }
+
+}
